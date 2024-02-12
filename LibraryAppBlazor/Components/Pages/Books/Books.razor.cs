a77a97adde8dfae8bfcf9d394432529a23319833
Silvia Sojčáková <silvia.sojcakova@riganti.cz>
2024-02-12 20:13:05 +0100

    Move checkout modal to component.

--- LibraryAppBlazor/Components/Pages/Books/Books.razor.cs
+++ LibraryAppBlazor/Components/Pages/Books/Books.razor.cs
@@ -12,30 +12,15 @@ namespace LibraryAppBlazor.Components.Pages.Books
         [Inject]
         IRepository<Book> bookRepository { get; set; }
 
-        [Inject]
-        IRepository<Member> memberRepository { get; set; }
-
-        [Inject]
-        IDbContextFactory<LibraryAppContext> dbContextFactory { get; set; }
-
         public IEnumerable<Book> BookList { get; set; } = new List<Book>();
-
-        public CheckOutRecord CheckoutData { get; set; } = new();
-
-        public Guid SelectedMemberId { get; set; } = Guid.Empty;
-
-        public Book BookToCheckout { get; set; } = new();
 
         private CheckoutDetailsModal childDetailsModal { get; set; }
 
-        public List<Member> MemberList { get; set; } = new();
-
+        private CheckoutModal childCheckoutModal { get; set; }
 
         protected override void OnInitialized()
         {
             BookList = bookRepository.List();
-            MemberList = memberRepository.List();
-            CheckoutData.CheckOutDate = DateTime.Now;
         }
 
         public void DeleteBook(Guid id)
@@ -50,38 +35,7 @@ namespace LibraryAppBlazor.Components.Pages.Books
         // checkout modal
         public void OpenCheckoutModal(Guid id)
         {
-            BookToCheckout = bookRepository.GetById(id);
-        }
-
-        public void BookCheckout()
-        {
-            if (BookToCheckout.IsAvailable)
-            {
-                CreateCheckoutRecord();
-
-                // update book
-                BookToCheckout.IsAvailable = false;
-                bookRepository.Edit(BookToCheckout);
-
-                UpdateBookList();
-            }
-        }
-
-        public void CreateCheckoutRecord()
-        {
-            var member = memberRepository.GetById(SelectedMemberId);
-            CheckoutData.Id = Guid.NewGuid();
-
-            using (var context = dbContextFactory.CreateDbContext())
-            {
-                context.Book.Attach(BookToCheckout);
-                CheckoutData.Book = BookToCheckout;
-                context.Member.Attach(member);
-                CheckoutData.Member = member;
-
-                context.CheckOutRecord.Add(CheckoutData);
-                context.SaveChanges();
-            }
+            childCheckoutModal.SelectBookToCheckout(id);
         }
 
         // detail modal
---
+++ LibraryAppBlazor/Components/Pages/CheckOuts/CheckoutModal.razor.cs
@@ -0,0 +1,73 @@
+﻿using LibraryAppBlazor.Data;
+using LibraryAppBlazor.Models;
+using LibraryAppBlazor.Repository;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace LibraryAppBlazor.Components.Pages.CheckOuts
+{
+    public partial class CheckoutModal
+    {
+        [Inject]
+        IRepository<Book> bookRepository { get; set; }
+
+        [Inject]
+        IRepository<Member> memberRepository { get; set; }
+
+        [Inject]
+        IDbContextFactory<LibraryAppContext> dbContextFactory { get; set; }
+
+        public Book BookToCheckout { get; set; } = new();
+
+        public Guid SelectedMemberId { get; set; } = Guid.Empty;
+
+        public List<Member> MemberList { get; set; } = new();
+
+        public CheckOutRecord CheckoutData { get; set; } = new();
+
+        [Parameter]
+        public EventCallback onBookCheckout { get; set; }
+
+        protected override void OnInitialized()
+        {
+            MemberList = memberRepository.List();
+            CheckoutData.CheckOutDate = DateTime.Now;
+        }
+
+        public void SelectBookToCheckout(Guid id) {
+            BookToCheckout = bookRepository.GetById(id);
+        }
+
+        public void BookCheckout()
+        {
+            if (BookToCheckout.IsAvailable)
+            {
+                CreateCheckoutRecord();
+
+                // update book
+                BookToCheckout.IsAvailable = false;
+                bookRepository.Edit(BookToCheckout);
+
+                onBookCheckout.InvokeAsync();
+            }
+        }
+
+        public void CreateCheckoutRecord()
+        {
+            var member = memberRepository.GetById(SelectedMemberId);
+            CheckoutData.Id = Guid.NewGuid();
+
+            using (var context = dbContextFactory.CreateDbContext())
+            {
+                context.Book.Attach(BookToCheckout);
+                CheckoutData.Book = BookToCheckout;
+                context.Member.Attach(member);
+                CheckoutData.Member = member;
+
+                context.CheckOutRecord.Add(CheckoutData);
+                context.SaveChanges();
+            }
+        }
+    }
+}
